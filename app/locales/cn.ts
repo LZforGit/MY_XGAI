8b43461dd7fc7fc1a6f7ad1d7c1dcd274feeef2a
LZZ <lz17635261610@gmail.com>
2023-06-12 14:30:23 +0800

    更改中文模块

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -1,7 +1,7 @@
 import { SubmitKey } from "../store/config";
 
 const cn = {
-  WIP: "该功能仍在开发中……",
+  WIP: "别点别点别点....还没开发呢...",
   Error: {
     Unauthorized:
       "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入你自己的 OpenAI API Key。",
