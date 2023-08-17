14d2c8e4ca00b087726457a5d9174a811b10a9d4
LZZ <lz17635261610@gmail.com>
2023-08-17 18:45:57 +0800

    0817更新代码3

--- app/components/ui-lib.tsx
+++ app/components/ui-lib.tsx
@@ -162,52 +162,52 @@ export type ToastProps = {
   onClose?: () => void;
 };
 
-export function Toast(props: ToastProps) {
-  return (
-    <div className={styles["toast-container"]}>
-      <div className={styles["toast-content"]}>
-        <span>{props.content}</span>
-        {props.action && (
-          <button
-            onClick={() => {
-              props.action?.onClick?.();
-              props.onClose?.();
-            }}
-            className={styles["toast-action"]}
-          >
-            {props.action.text}
-          </button>
-        )}
-      </div>
-    </div>
-  );
-}
-
-export function showToast(
-  content: string,
-  action?: ToastProps["action"],
-  delay = 3000,
-) {
-  const div = document.createElement("div");
-  div.className = styles.show;
-  document.body.appendChild(div);
-
-  const root = createRoot(div);
-  const close = () => {
-    div.classList.add(styles.hide);
-
-    setTimeout(() => {
-      root.unmount();
-      div.remove();
-    }, 300);
-  };
-
-  setTimeout(() => {
-    close();
-  }, delay);
-
-  root.render(<Toast content={content} action={action} onClose={close} />);
-}
+// export function Toast(props: ToastProps) {
+//   return (
+//     <div className={styles["toast-container"]}>
+//       <div className={styles["toast-content"]}>
+//         <span>{props.content}</span>
+//         {props.action && (
+//           <button
+//             onClick={() => {
+//               props.action?.onClick?.();
+//               props.onClose?.();
+//             }}
+//             className={styles["toast-action"]}
+//           >
+//             {props.action.text}
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+//
+// export function showToast(
+//   content: string,
+//   action?: ToastProps["action"],
+//   delay = 3000,
+// ) {
+//   const div = document.createElement("div");
+//   div.className = styles.show;
+//   document.body.appendChild(div);
+//
+//   const root = createRoot(div);
+//   const close = () => {
+//     div.classList.add(styles.hide);
+//
+//     setTimeout(() => {
+//       root.unmount();
+//       div.remove();
+//     }, 300);
+//   };
+//
+//   setTimeout(() => {
+//     close();
+//   }, delay);
+//
+//   root.render(<Toast content={content} action={action} onClose={close} />);
+// }
 
 export type InputProps = React.HTMLProps<HTMLTextAreaElement> & {
   autoHeight?: boolean;
